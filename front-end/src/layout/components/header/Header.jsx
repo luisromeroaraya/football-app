54b5708d83efa83547abbdb01e999b80708c072f
Sebastian Herrera <sebastian@tapioview.com>
2022-10-23 00:16:31 +0200

    remove old react app

--- front-end/src/layout/components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <div className="w-full h-24 flex justify-center items-center">
-      <h1 className="text-2xl font-bold ">Pichangapp</h1>
-    </div>
-  );
-};
-
-export default Header;
