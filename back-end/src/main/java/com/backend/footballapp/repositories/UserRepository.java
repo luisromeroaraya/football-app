fcfa1b986292c17946bee176942ccad44a4eb2c0
luisromeroaraya <luisromeroaraya@gmail.com>
2022-10-30 16:48:12 +0100

    Fixed TeamDTO filename in backend

--- back-end/src/main/java/com/backend/footballapp/repositories/UserRepository.java
+++ back-end/src/main/java/com/backend/footballapp/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.backend.footballapp.repositories;
 
 import com.backend.footballapp.models.entities.User;
-import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
